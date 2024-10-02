696908072b08eabb43bca1ca60551fddfc81df4f
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2024-10-02 13:35:35 +0700

    commit yeaaay

---
+++ Pemilihan2Percobaan3_18.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class Pemilihan2Percobaan3_18 {
+
+    public static void main (String [] args){
+        Scanner input18 = new Scanner(System.in);
+
+        String Kategori;
+        int penghasilan,gajiBersih;
+        double pajak;
+
+        System.out.print(" Masukkan Kategori : ");
+        Kategori = input18.nextLine();
+        System.out.print("Masukkan besarnya penghasilan : ");
+        penghasilan = input18.nextInt();
+
+        if (Kategori.equalsIgnoreCase("pekerja")){
+            if (penghasilan <=2000000)
+            pajak = 0.1;
+            else if (penghasilan <=3000000)
+            pajak = 0.15;
+            else
+            pajak = 0.2;
+            gajiBersih = (int) (penghasilan -(pajak * penghasilan));
+            System.out.println("penghasilan bersih" + gajiBersih);
+        }else if (Kategori.equalsIgnoreCase("pebisnis")) {
+            if (penghasilan <=2500000)
+            pajak = 0.15;
+            else if (penghasilan <=3500000)
+            pajak =0.2;
+            else
+            pajak = 0.25;
+            gajiBersih = (int) (penghasilan -(pajak * penghasilan));
+            System.out.println("penghasilan bersih" + gajiBersih);
+
+        }else
+            System.out.println("masukan kategori salah");
+    }
+}
